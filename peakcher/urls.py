0b378488e584bd1f5f3e5e5f054831735693b975
Ryan Taheri <sailantht@gmail.com>
2021-04-08 15:50:52 +0430

    5.setting up media folder

--- peakcher/urls.py
+++ peakcher/urls.py
@@ -16,6 +16,9 @@ Including another URLconf
 from django.contrib import admin
 from django.urls import path
 from django.conf.urls import include
+from django.conf import settings
+from django.conf.urls.static import static
+
 from feed import urls as feeds_urls
 
 
@@ -23,3 +26,7 @@ urlpatterns = [
     path('admin/', admin.site.urls),
     path('',include(feeds_urls, namespace= 'feed')),
 ]
+
+
+if settings.DEBUG:
+    urlpatterns += static(settings.MEDIA_URL, document_root=settings.MEDIA_ROOT)
