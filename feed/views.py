94491d82a099693cd18c818fc3356e0f5d055e0e
Ryan Taheri <sailantht@gmail.com>
2021-04-09 12:45:54 +0430

    12.Django message

--- feed/views.py
+++ feed/views.py
@@ -1,4 +1,5 @@
 from django.shortcuts import render
+from django.contrib import messages
 from django.views.generic import TemplateView, DetailView, FormView
 
 
@@ -24,10 +25,17 @@ class AddPostView(FormView):
    form_class = PostForm
    success_url = "/"
 
+   def dispatch(self, request, *args, **kwargs):
+      # We need this because message needs request and form_valid doesn't accept request
+      self.request = request
+      return super().dispatch(request, *args, **kwargs)
+
    def form_valid(self, form):
       # Create a new post
       new_obj = Post.objects.create(
          text = form.cleaned_data['text'],
          image = form.cleaned_data['image']
       )
+      # Show a message after submitting the form
+      messages.add_message(self.request, messages.SUCCESS, 'Your post was successful')
       return super().form_valid(form)
