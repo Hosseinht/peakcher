ae423991acc9b81ab62cc5728e21af6ca4c280a3
Ryan Taheri <sailantht@gmail.com>
2021-04-09 12:25:24 +0430

    10.Adding an upload form

--- feed/views.py
+++ feed/views.py
@@ -1,6 +1,9 @@
 from django.shortcuts import render
-from django.views.generic import TemplateView, DetailView
+from django.views.generic import TemplateView, DetailView, FormView
+
+
 from .models import Post
+from .forms import PostForm
 
 
 class HomePageView(TemplateView):
@@ -14,3 +17,17 @@ class HomePageView(TemplateView):
 class PostDetailView(DetailView):
    template_name= "detail.html"
    model = Post
+
+
+class AddPostView(FormView):
+   template_name = "new_post.html"
+   form_class = PostForm
+   success_url = "/"
+
+   def form_valid(self, form):
+      # Create a new post
+      new_obj = Post.objects.create(
+         text = form.cleaned_data['text'],
+         image = form.cleaned_data['image']
+      )
+      return super().form_valid(form)
